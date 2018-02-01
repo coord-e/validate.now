53b3d82282f7964cf4f1dc095064ebbe6171fd96
coord.e <me@coord-e.com>
2018-02-01 22:48:02 +0900

    Specify input and output path

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,16 @@ import resolve from "rollup-plugin-node-resolve"
 import uglify from "rollup-plugin-uglify"
 import postcss from 'rollup-plugin-postcss'
 
+const dist = 'dist/'
+const src = 'src/'
+
 export default {
+  input: path.join(src, 'js/main.jsx'),
+	output: {
+		file: path.join(dist, 'js/bundle.js'),
+		format: 'iife',
+		sourcemap: true
+	},
   plugins: [
     postcss({
       modules: true
