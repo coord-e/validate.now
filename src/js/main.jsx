9f991aa6b4a88feec220680f0956502dc7ea2c1c
coord.e <me@coord-e.com>
2018-02-02 01:06:10 +0900

    Remove unnecessary code and add editor textarea

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,44 +1,28 @@
 import { h, app } from "hyperapp"
-import { Link, Route, location } from "@hyperapp/router"
 
 import styles from "../css/style.css"
 
-const Home = () => <h2>Hom</h2>
-const About = () => <h2>About</h2>
-
 const state = {
-  count: 0,
-  location: location.state
+  text: "",
 }
 
 const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 }),
-  location: location.actions
+  update: text => state => ({ text }),
 }
+
+const CodeEditor = ({text, update}) => (
+  <textarea placeholder="code: this"
+            value={text}
+            oninput={e => update(e.target.value)}
+          />
+)
 
 const view = (state, actions) => (
   <div>
-    <header>
-      <ul>
-          <li>
-            <Link to="/">Hom</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-    </header>
     <main>
-      <h1 class={state.count > 10 ? styles.title : ""}>{state.count}</h1>
-      <button onclick={actions.down}>-</button>
-      <button onclick={actions.up}>+</button>
+      <CodeEditor text={state.text} update={actions.update} />
     </main>
-    <Route path="/" render={Home} />
-    <Route path="/about" render={About} />
   </div>
 )
 
 const main = app(state, actions, view, document.body)
-
-const unsubscribe = location.subscribe(main.location)
