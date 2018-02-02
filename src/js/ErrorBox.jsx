d7f03e11a846ce53b59ebc30c5dcd4cedcffb13b
coord.e <me@coord-e.com>
2018-02-03 00:22:51 +0900

    Some adjustments

--- src/js/ErrorBox.jsx
+++ src/js/ErrorBox.jsx
@@ -3,14 +3,16 @@ import { h } from "hyperapp"
 import errorStyles from "../css/error.css"
 
 export default ({error, text="Valid!", toggle_hide}) => (
-  <div class={errorStyles.container}>
-    {
-      error
-        ? <textarea readonly class={errorStyles.view}>{JSON.stringify(error, undefined, 2)}</textarea>
-        : <div>
-          <span class={errorStyles.valid}>{text}</span>
-          <button class={errorStyles.toggle} onclick={toggle_hide}>Hide/Show schema</button>
-        </div>
-    }
+  <div>
+    <div class={errorStyles.container}>
+      {
+        error
+          ? <textarea readonly class={errorStyles.view}>{JSON.stringify(error, undefined, 2)}</textarea>
+          : <div>
+            <span class={errorStyles.valid}>{text}</span>
+          </div>
+      }
+    </div>
+    <button class={errorStyles.toggle} onclick={toggle_hide}>Hide/Show schema</button>
   </div>
 )
